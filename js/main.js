a05488153f8064a5e185c8c3cd9f4f11ec36b5c8
RT <rotyflo@gmail.com>
2017-11-13 22:59:20 +0900

    Add start, pause and reset functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 let timeDisplay = document.getElementById("time-display");
-let seconds = 10;
-let countDown = setInterval(timer, 1000);
+let startButton = document.getElementById("start");
+let pauseButton = document.getElementById("pause");
+let resetButton = document.getElementById("reset");
+let seconds = 1500;
+let countDown = null;
+
+startButton.addEventListener("click", function() {
+  countDown = setInterval(timer, 1000);
+  startButton.style.display = "none";
+  pauseButton.style.display = "inline";
+});
+
+pauseButton.addEventListener("click", function() {
+  clearInterval(countDown);
+  pauseButton.style.display = "none";
+  startButton.style.display = "inline";
+});
+
+resetButton.addEventListener("click", function() {
+  clearInterval(countDown);
+  timeDisplay.innerText = "25:00";
+  seconds = 1500;
+  pauseButton.style.display = "none";
+  startButton.style.display = "inline";
+});
 
 function timer() {
   let m = Math.floor(seconds / 60);
