ee60b5f779d392ae973d4684066d63666ad1b58f
RT <me@rtflores.com>
2021-11-04 18:52:10 -0700

    fix incorrect App import

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'terminal.css';
-import App from './components/App';
+import App from './containers/App';
 import { Provider } from 'react-redux';
 import store from './store';
 
